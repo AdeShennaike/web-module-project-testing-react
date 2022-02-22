0914ce076c0dc74fafc90cfad53b14e1e157a3fb
Ade <shennaike2386@hotmail.com>
2022-02-22 01:56:46 -0500

    finished tests for episode component

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,8 +3,45 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Episode from './../Episode';
 
-test("renders without error", () => { });
+const testData = {
+    id: 1,
+    image: '',
+    name: 'My favorite episode',
+    season: 1,
+    number: 4,
+    summary: 'We need season 5 already, sheesh!',
+    runtime: '50 minutes' 
+}
 
-test("renders the summary test passed as prop", () => { });
+const imageData = {
+    id: 1,
+    image: null,
+    name: 'My favorite episode',
+    season: 1,
+    number: 4,
+    summary: 'We need season 5 already, sheesh!',
+    runtime: '50 minutes' 
+}
 
-test("renders default image when image is not defined", () => { });
+test("renders without error", () => { 
+    render(<Episode episode ={['Stranger Example']}/>)
+});
+
+test("renders the summary test passed as prop", () => { 
+    render(<Episode episode = {testData}/>)
+
+    const summary = screen.queryByText(/need/i)
+
+    expect(summary).toBeVisible
+    expect(summary).toBeInTheDocument
+    expect(summary).toHaveTextContent('season')
+});
+
+test("renders default image when image is not defined", () => { 
+    <Episode episode = {imageData}/>
+
+    const altTag = screen.queryByText('https://i.ibb.co/2FsfXqM/stranger-things.png')
+
+    expect(altTag).toBeInTheDocument
+
+});
